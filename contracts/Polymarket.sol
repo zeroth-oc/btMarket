90bb1ab315bed3e363f6ccfc3283a04139d1196a
txcc <64108848+zeroth-oc@users.noreply.github.com>
2022-12-28 21:40:23 +0100

    Update Polymarket.sol

--- contracts/Polymarket.sol
+++ contracts/Polymarket.sol
@@ -1,23 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Polymarket {
+contract BetMarket {
     address public owner;
-    address public polyToken;
+    uint256 public totalMarkets = 0;
 
-    uint256 public totalQuestions = 0;
+    //constants 
+    uint tax = 100000000000000; // tax 0.0001 eth 
 
-    constructor(address _polyToken) {
+    //constants
+    uint internal minimumBet = 100000000000000000; // min 0.1 eth
+    
+    constructor() {
         owner = msg.sender;
-        polyToken = _polyToken;
     }
+    mapping(uint256 => Markets) public markets;
 
-    mapping(uint256 => Questions) public questions;
 
-    struct Questions {
+    struct Markets {
         uint256 id;
-        string question;
+        string  market;
         uint256 timestamp;
         uint256 endTimestamp;
         address createdBy;
@@ -40,10 +42,10 @@ contract Polymarket {
 
     mapping(address => uint256) public winningAmount;
     address[] public winningAddresses;
-
-    event QuestionCreated(
+    
+    event MarketCreated(
         uint256 id,
-        string question,
+        string market,
         uint256 timestamp,
         address createdBy,
         string creatorImageHash,
@@ -52,34 +54,43 @@ contract Polymarket {
         uint256 totalNoAmount
     );
 
-    function createQuestion(
-        string memory _question,
+    
+    receive() external payable{}
+
+
+
+    function getBalance() public view returns(uint256){
+        return address(this).balance;
+    }
+
+    
+    function createMarket(
+        string memory _market,
         string memory _creatorImageHash,
         string memory _description,
         string memory _resolverUrl,
         uint256 _endTimestamp
     ) public {
         require(msg.sender == owner, "Unauthorized");
-
         uint256 timestamp = block.timestamp;
 
-        Questions storage question = questions[totalQuestions];
+        Markets storage market = markets[totalMarkets];
+        market.id = totalMarkets++;
+        market.market = _market;
+        market.timestamp = timestamp;
+        market.createdBy = msg.sender;
+        market.creatorImageHash = _creatorImageHash;
+        market.totalAmount = 0;
+        market.totalYesAmount = 0;
+        market.totalNoAmount = 0;
+        market.description = _description;
+        market.resolverUrl = _resolverUrl;
+        market.endTimestamp = _endTimestamp;
 
-        question.id = totalQuestions++;
-        question.question = _question;
-        question.timestamp = timestamp;
-        question.createdBy = msg.sender;
-        question.creatorImageHash = _creatorImageHash;
-        question.totalAmount = 0;
-        question.totalYesAmount = 0;
-        question.totalNoAmount = 0;
-        question.description = _description;
-        question.resolverUrl = _resolverUrl;
-        question.endTimestamp = _endTimestamp;
 
-        emit QuestionCreated(
-            totalQuestions,
-            _question,
+        emit MarketCreated(
+            totalMarkets,
+            _market,
             timestamp,
             msg.sender,
             _creatorImageHash,
@@ -88,89 +99,96 @@ contract Polymarket {
             0
         );
     }
+    
+    function addYesBet(uint256 _marketId) public payable {
+        require(msg.value != 0, "you cant stake 0 wei");
+        //bet must be above a certain minimum 
+        require(msg.value >= minimumBet);
+        Markets storage market = markets[_marketId];
+        (bool sent, ) = address(this).call{value: msg.value}("");
+        require(sent, "failed to send");
 
-    function addYesBet(uint256 _questionId, uint256 _value) public payable {
-        require(_questionId == 0, "Question ID cannot be null");
-        Questions storage question = questions[_questionId];
-        ERC20(polyToken).transferFrom(msg.sender, address(this), _value);
         AmountAdded memory amountAdded = AmountAdded(
             msg.sender,
-            _value,
+            msg.value,
             block.timestamp
         );
 
-        question.totalYesAmount += _value;
-        question.totalAmount += _value;
-        question.yesCount.push(amountAdded);
+        market.totalYesAmount += msg.value;
+        market.totalAmount += msg.value;
+        market.yesCount.push(amountAdded);
     }
 
-    function addNoBet(uint256 _questionId, uint256 _value) public payable {
-        require(_questionId == 0, "Question ID cannot be null");
-        Questions storage question = questions[_questionId];
-        ERC20(polyToken).transferFrom(msg.sender, address(this), _value);
+    function addNoBet(uint256 _marketId) public payable {
+        require(msg.value != 0, "you cant stake 0 wei");
+        //bet must be above a certain minimum 
+        require(msg.value >= minimumBet);
+        Markets storage market = markets[_marketId];
+        (bool sent, ) = address(this).call{value: msg.value}("");
+        require(sent, "failed to send");
+
         AmountAdded memory amountAdded = AmountAdded(
             msg.sender,
-            _value,
+            msg.value,
             block.timestamp
         );
 
-        question.totalNoAmount += _value;
-        question.totalAmount += _value;
-        question.noCount.push(amountAdded);
+        market.totalNoAmount += msg.value;
+        market.totalAmount += msg.value;
+        market.noCount.push(amountAdded);
     }
 
-    function getGraphData(uint256 _questionId)
+    function getGraphData(uint256 _marketId)
         public
         view
         returns (AmountAdded[] memory, AmountAdded[] memory)
     {
-        Questions storage question = questions[_questionId];
-        return (question.yesCount, question.noCount);
+        Markets storage market = markets[_marketId];
+        return (market.yesCount, market.noCount);
     }
 
-    function distributeWinningAmount(uint256 _questionId, bool eventOutcome)
+
+    function distributeWinningAmount(uint256 _marketId, bool eventOutcome)
         public
         payable
     {
         require(msg.sender == owner, "Unauthorized");
 
-        Questions storage question = questions[_questionId];
+        Markets storage market = markets[_marketId];
         if (eventOutcome) {
-            for (uint256 i = 0; i < question.yesCount.length; i++) {
-                uint256 amount = (question.totalNoAmount *
-                    question.yesCount[i].amount) / question.totalYesAmount;
-                winningAmount[question.yesCount[i].user] += (amount +
-                    question.yesCount[i].amount);
-                winningAddresses.push(question.yesCount[i].user);
+            for (uint256 i = 0; i < market.yesCount.length; i++) {
+                uint256 amount = ((market.totalNoAmount *
+                    market.yesCount[i].amount) / market.totalYesAmount) - tax;
+                winningAmount[market.yesCount[i].user] += (amount +
+                    market.yesCount[i].amount);
+                winningAddresses.push(market.yesCount[i].user);
             }
 
             for (uint256 i = 0; i < winningAddresses.length; i++) {
                 address payable _address = payable(winningAddresses[i]);
-                ERC20(polyToken).transfer(_address, winningAmount[_address]);
+                bool sent = (_address).send(winningAmount[_address]);
+                require(sent, "failed to distribute");
                 delete winningAmount[_address];
             }
             delete winningAddresses;
         } else {
-            for (uint256 i = 0; i < question.noCount.length; i++) {
-                uint256 amount = (question.totalYesAmount *
-                    question.noCount[i].amount) / question.totalNoAmount;
-                winningAmount[question.noCount[i].user] += (amount +
-                    question.noCount[i].amount);
-                winningAddresses.push(question.noCount[i].user);
+            for (uint256 i = 0; i < market.noCount.length; i++) {
+                uint256 amount = ((market.totalYesAmount *
+                    market.noCount[i].amount) / market.totalNoAmount) - tax;
+                winningAmount[market.noCount[i].user] += (amount +
+                    market.noCount[i].amount);
+                winningAddresses.push(market.noCount[i].user);
             }
 
             for (uint256 i = 0; i < winningAddresses.length; i++) {
                 address payable _address = payable(winningAddresses[i]);
-                ERC20(polyToken).transfer(_address, winningAmount[_address]);
+                bool sent = (_address).send(winningAmount[_address]);
+                require(sent, "failed to distribute");
                 delete winningAmount[_address];
             }
             delete winningAddresses;
         }
-        question.eventCompleted = true;
+        market.eventCompleted = true;
     }
-
-    function isAdmin() public view returns (bool) {
-        if (msg.sender == owner) return true;
-        else return false;
-    }
+  
 }
